31c20686021723c40341ad0eda026a6bbb5e68ad
Farbod Heydari <kharazmifrequency@gmail.com>
2024-01-04 07:50:30 -0600

    show top results

--- lib/actions/tag.actions.ts
+++ lib/actions/tag.actions.ts
@@ -70,3 +70,17 @@ export async function getQustionsByTagId(params: GetQuestionsByTagIdParams) {
     throw error;
   }
 }
+export async function getTopPopularTags() {
+  try {
+    connectToDatabase();
+    const popularTags = await Tag.aggregate([
+      { $project: { name: 1, numberOfQuestions: { $size: "$questions" } } },
+      { $sort: { numberOfQuestions: -1 } },
+      { $limit: 5 },
+    ]);
+    return popularTags;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
